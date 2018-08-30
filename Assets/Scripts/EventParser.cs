5dd0ac6d9f154e0f5d73d4febad4741001d513f7
Roger Anguera <ranguera@gmail.com>
2018-08-30 16:11:56 -0700

    added event parser and event file sample

---
+++ Assets/Scripts/EventParser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventParser : MonoBehaviour {
+
+	void Start () {
+        TextAsset events = Resources.Load("events") as TextAsset;
+        print(events.text);
+        Events sc = JsonUtility.FromJson<Events>(events.text);
+        print(sc.events.Length);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
