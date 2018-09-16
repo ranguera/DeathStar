a12b94023ca1138a9bd63aba2b5764977c8dbf7c
Roger Anguera <ranguera@gmail.com>
2018-09-15 22:29:28 -0700

    line renderers. re-check and bake mesh

--- Assets/Scripts/RenderSensors.cs
+++ Assets/Scripts/RenderSensors.cs
@@ -9,13 +9,17 @@ namespace DeathStar
     {
         public GameObject sensor;
         public GameObject parent;
+        public LineRenderer lr;
         public Texture2D heatmap;
         public Text eventnum;
         public Text eventID;
         public float scaleFactor;
+        public Material lineMaterial;
+        private Color colorOFF;
+        public Color background;
+        public Color lineColor;
 
         private List<Renderer> renderers;
-        private Color colorOFF;
         private SensorCoordinates sc;
         private Events evs;
 
@@ -29,13 +33,14 @@ namespace DeathStar
             print(sc.y.Length);
             print(sc.z.Length);
 
-            TextAsset events = Resources.Load("events3") as TextAsset;
+            TextAsset events = Resources.Load("events-bright") as TextAsset;
             print(events.text);
             evs = JsonUtility.FromJson<Events>(events.text);
             print(evs.events.Length);
 
             this.renderers = new List<Renderer>();
             this.colorOFF = sensor.GetComponent<Renderer>().sharedMaterial.color;
+            this.background = new Color(39f / 255f, 39f / 255f, 39f / 255f, 12f/255f);
 
             for (int i = 0; i < sc.x.Length; i++)
             {
@@ -51,18 +56,37 @@ namespace DeathStar
         {
             List<int> ids = new List<int>();
             List<int> charges = new List<int>();
+            
             for (int i = 0; i < evs.events.Length; i++)
             {
                 yield return new WaitForSeconds(.8f);
                 ids.Clear();
                 charges.Clear();
+                int line_index = 0;
                 eventnum.text = evs.events[i].evento.ToString();
                 eventID.text = evs.events[i].ID.ToString();
+                this.lr.positionCount = 1 + evs.events[i].sensor.Length*2;
 
                 for (int j = 0; j < evs.events[i].sensor.Length; j++)
                 {
                     ids.Add(evs.events[i].sensor[j]);
                     charges.Add(evs.events[i].charge[j]);
+                    //print(lr.positionCount);
+                    //print(1 + j * 2);
+                    if (evs.events[i].charge[j] > 1000)
+                    {
+                        if (ids[j] < this.renderers.Count)
+                        {
+                            this.lr.SetPosition(line_index, this.renderers[evs.events[i].sensor[j]].gameObject.transform.position);
+                            this.lr.SetPosition((line_index + 1), Vector3.zero);
+                        }
+                        else
+                        {
+                            this.lr.SetPosition((line_index), Vector3.zero);
+                            this.lr.SetPosition((line_index + 1), Vector3.zero);
+                        }
+                        line_index += 2;
+                    }
                 }
 
                 StartCoroutine(Flash(ids, charges));
@@ -74,6 +98,7 @@ namespace DeathStar
             MaterialPropertyBlock props = new MaterialPropertyBlock();
             Color[] flashcolors = new Color[ids.Count];
             float t = Time.time;
+            this.lineMaterial.SetColor("_TintColor", lineColor);
 
             for (int i = 0; i < ids.Count; i++)
             {
@@ -96,6 +121,7 @@ namespace DeathStar
                         this.renderers[ids[i]].SetPropertyBlock(props);
                     }
                 }
+                this.lineMaterial.SetColor("_TintColor", Color.Lerp(lineColor, this.background, (Time.time - t) * 2f));
 
                 yield return null;
             }
